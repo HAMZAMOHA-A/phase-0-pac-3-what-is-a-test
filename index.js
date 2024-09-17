e659ce65d69d1e46d0f1717527271c0764e76e9e
hamzamoha-a <hamzashukri29@gmail.com>
2024-09-17 12:58:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan";
+const height = 39;
+const message = `{Susan} is {39} inches tall`;
 
 module.exports = {name, height, message};
 console.log(message);
